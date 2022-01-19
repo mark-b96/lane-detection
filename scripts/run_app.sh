3545e47eb72722d33f0fcd38569fa3c07b81519a
mark-b96 <mark_b96@protonmail.com>
2022-01-19 16:15:57 +0000

    Checking for gpu availability

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,3 +1,10 @@
 #!bin/bash
 
-docker run -it -v /home/ubuntu:/home/ubuntu --hostname $HOSTNAME --name lane-det-container lane-det-img
+if ! type "nvidia-smi" > /dev/null; then
+    echo "Running on CPU"
+    docker run -it -v /home/ubuntu:/home/ubuntu --hostname $HOSTNAME --name lane-det-container lane-det-img
+else
+    echo "Running on GPU"
+    docker run --gpus all -it -v /home/ubuntu:/home/ubuntu --hostname $HOSTNAME --name lane-det-container lane-det-img
+fi
+
