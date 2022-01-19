1ca1836d297191271973313319e9edbdc0894dcd
mark-b96 <mark_b96@protonmail.com>
2022-01-19 19:54:32 +0000

    Updated run script

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,10 +1,11 @@
-#!bin/bash
+#!/bin/bash
+
+docker pull markb96/lane-det-repo:myfirstpush
 
 if ! type "nvidia-smi" > /dev/null; then
     echo "Running on CPU"
-    docker run -it -v /home/ubuntu:/home/ubuntu --hostname $HOSTNAME --name lane-det-container lane-det-img
+    docker run -it -v /home/${SUDO_USER}:/home/${SUDO_USER} --hostname $HOSTNAME --name lane-det-container markb96/lane-det-repo:myfirstpush
 else
     echo "Running on GPU"
-    docker run --gpus all -it -v /home/ubuntu:/home/ubuntu --hostname $HOSTNAME --name lane-det-container lane-det-img
+    docker run --gpus all -it -v /home/${SUDO_USER}:/home/${SUDO_USER} --hostname $HOSTNAME --name lane-det-container markb96/lane-det-repo:myfirstpush
 fi
-
