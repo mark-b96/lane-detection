001b3af945d5a8f23a4813187fee122d54a29834
mark-b96 <mark_b96@protonmail.com>
2022-01-21 16:00:59 +0000

    non-interactive docker cmd

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -4,19 +4,18 @@ AWS_ACCOUNT_ID=739366754163
 AWS_REGION=eu-west-2
 ECR_DOCKER_IMG=lane-det-img:latest
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#./aws/install
 
+#docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_REGION}) ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_REGION}) ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
-
-docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG}
+#docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG}
 
 if ! type "nvidia-smi" > /dev/null; then
     echo "Running on CPU"
-    docker run -it -v /home/${SUDO_USER}:/home/${SUDO_USER} --hostname $HOSTNAME --name lane-det-container ${ECR_DOCKER_IMG}
+    docker run -v /home/${SUDO_USER}:/home/${SUDO_USER} --hostname $HOSTNAME --name lane-det-container ${ECR_DOCKER_IMG}
 else
     echo "Running on GPU"
-    docker run --gpus all -it -v /home/${SUDO_USER}:/home/${SUDO_USER} --hostname $HOSTNAME --name lane-det-container ${ECR_DOCKER_IMG}
+    docker run --gpus all -v /home/${SUDO_USER}:/home/${SUDO_USER} --hostname $HOSTNAME --name lane-det-container ${ECR_DOCKER_IMG}
 fi
