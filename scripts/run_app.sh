7d4bc56deff04a23f6def12f768d5b27800733ef
mark-b96 <mark_b96@protonmail.com>
2022-01-23 16:15:33 +0000

    Update pwd

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -8,14 +8,23 @@ ECR_DOCKER_IMG=lane-det-img:latest
 #unzip awscliv2.zip
 #./aws/install
 
-#docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_REGION}) ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+#docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_REGION}) \
+# ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 #docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG}
 
+SCRIPT_RELATIVE_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd $SCRIPT_RELATIVE_DIR
+cd ../
+
 if ! type "nvidia-smi" > /dev/null; then
     echo "Running on CPU"
-    docker run --rm -v /home/${SUDO_USER}:/home/${SUDO_USER} -v /opt:/opt --hostname $HOSTNAME --name lane-det-container ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG} /bin/bash -c "cd ${PWD}; python3.8 lane_detector.py"
+    docker run --rm -v /home/${SUDO_USER}:/home/${SUDO_USER} -v /opt:/opt --hostname $HOSTNAME \
+          --name lane-det-container ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG} \
+           /bin/bash -c "cd ${PWD}; python3.8 lane_detector.py"
 else
     echo "Running on GPU"
-    docker run --rm --gpus all -v /home/${SUDO_USER}:/home/${SUDO_USER} -v /opt:/opt --hostname $HOSTNAME --name lane-det-container ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG} /bin/bash -c "cd ${PWD}; python3.8 lane_detector.py"
+    docker run --rm --gpus all -v /home/${SUDO_USER}:/home/${SUDO_USER} -v /opt:/opt --hostname $HOSTNAME \
+           --name lane-det-container ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_DOCKER_IMG} \
+           /bin/bash -c "cd ${PWD}; python3.8 lane_detector.py"
 fi
